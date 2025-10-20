cd1088c0af1891c40333961acc69176cdc8420f5
Javiertries2code <superjabo@live.com>
2025-10-20 20:32:19 +0200

    se ha creado is_binary

--- parsers/eval_words.c
+++ parsers/eval_words.c
@@ -1,12 +1,12 @@
 #include "../mini.h"
-void reassign_value(char **old, char *new)
+
+void	reassign_value(char **old, char *new)
 {
-	if(*old && *old != new)  // Only free if different pointer
+	if (*old && *old != new) // Only free if different pointer
 	{
 		free(*old);
 	}
 	*old = new;
-
 }
 
 bool	check_prev(t_data *data, t_token *token, char *word)
@@ -30,47 +30,48 @@ bool	check_prev(t_data *data, t_token *token, char *word)
 
 int	eval_red(t_data *data, t_token *token, char *word)
 {
-    if (!ft_strncmp(word, ">>", 2))
-    {
-        data->l_ff[token->row] = token->pos;
-        token->token_op = APPEND;
-        token->value = ft_strdup(word);  // CAMBIO: Hacer copia
-        return (true);
-    }
-    else if (!ft_strncmp(word, "<<", 2))
-    {
-        data->l_hd[token->row] = token->pos;
-        token->token_op = HEREDOC;
-        token->value = ft_strdup(word);  // CAMBIO: Hacer copia
-        return (true);
-    }
-    else if (!ft_strncmp(word, ">", 1))
-    {
-        data->l_for[token->row] = token->pos;
-        token->token_op = RED_FORWD;
-        token->value = ft_strdup(word);  // CAMBIO: Hacer copia
-        return (true);
-    }
-    else if (!ft_strncmp(word, "<", 1))
-    {
-        data->l_back[token->row] = token->pos;
-        token->token_op = RED_BACKWD;
-        token->value = ft_strdup(word);  // CAMBIO: Hacer copia
-        return (true);
-    }
-    return (false);
+	if (!ft_strncmp(word, ">>", 2))
+	{
+		data->l_ff[token->row] = token->pos;
+		token->token_op = APPEND;
+		token->value = ft_strdup(word); // CAMBIO: Hacer copia
+		return (true);
+	}
+	else if (!ft_strncmp(word, "<<", 2))
+	{
+		data->l_hd[token->row] = token->pos;
+		token->token_op = HEREDOC;
+		token->value = ft_strdup(word); // CAMBIO: Hacer copia
+		return (true);
+	}
+	else if (!ft_strncmp(word, ">", 1))
+	{
+		data->l_for[token->row] = token->pos;
+		token->token_op = RED_FORWD;
+		token->value = ft_strdup(word); // CAMBIO: Hacer copia
+		return (true);
+	}
+	else if (!ft_strncmp(word, "<", 1))
+	{
+		data->l_back[token->row] = token->pos;
+		token->token_op = RED_BACKWD;
+		token->value = ft_strdup(word); // CAMBIO: Hacer copia
+		return (true);
+	}
+	return (false);
 }
-
 
 /**
  * @brief Compares two strings lexicographically.
- * 
- * This function compares the string pointed to by s1 to the string pointed to by s2.
- * The comparison is done using unsigned characters, so that '\200' is greater than '\0'.
- * 
+ *
+
+	* This function compares the string pointed to by s1 to the string pointed to by s2.
+ * The comparison is done using unsigned characters,
+	so that '\200' is greater than '\0'.
+ *
  * @param s1 Pointer to the first null-terminated string to be compared
  * @param s2 Pointer to the second null-terminated string to be compared
- * 
+ *
  * @return An integer less than, equal to, or greater than zero if s1 is found,
  *         respectively, to be less than, to match, or to be greater than s2.
  *         - < 0: s1 is lexicographically less than s2
@@ -86,12 +87,28 @@ int	ft_strcmp(const char *s1, const char *s2)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
+
+bool	is_binary(t_data *data, t_token *token, char *word)
+{
+	if (ft_strncmp("./", word, 2))
+	{
+		if ( word[2] != 0 && ft_isalnum(word[2]))
+		{
+			token->token_op = BINARY;
+			token->value = word;
+			data->command_set[token->row] = true;
+			return (true);
+		}
+		else
+			return false;
+	}
+	return false;
+}
 bool	eval_builtin(t_data *data, t_token *token, char *word)
 {
 	char	**built;
 	int		i;
 
-	
 	if (token->prev && (token->prev->token_op >= 1
 			&& token->prev->token_op <= 4))
 		return (false);
