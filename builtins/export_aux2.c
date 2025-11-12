7aee355e719386e80c603a73e6ad397d46a71d6b
Javiertries2code <superjabo@live.com>
2025-11-12 20:10:30 +0100

    no leaks related to exporting, it seems to be fully funtional

--- builtins/export_aux2.c
+++ builtins/export_aux2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   export_aux2.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marregi- <marregi-@student.42.fr>          +#+  +:+       +#+        */
+/*   By: havr <havr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/27 12:58:10 by marregi-          #+#    #+#             */
-/*   Updated: 2025/11/10 12:41:53 by marregi-         ###   ########.fr       */
+/*   Updated: 2025/11/12 20:04:08 by havr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -85,4 +85,5 @@ void	manage_export(t_token *list, t_data *data, bool assign)
 	}
 	free(env[0]);
 	free(env[1]);
+	free(env); 
 }
