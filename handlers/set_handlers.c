642be7d0a26908daa167c28d5994c2d440838138
mikytoful <mikel.arregi07@gmail.com>
2025-11-14 17:59:46 +0100

    handlers ctrl+c

--- handlers/set_handlers.c
+++ handlers/set_handlers.c
@@ -25,7 +25,7 @@ void	handle_heredoc_ctrl_c(int signal)
 	(void)signal;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
-	rl_replace_line("CTRL-C PRESSED-HEREDOC\n", 0);
+	rl_replace_line("CTRL-C PRESSED-HEREDOC\n", 24);
 	rl_redisplay();
 }
 
@@ -34,7 +34,7 @@ void	handler_ctrl_c(int signal)
 	(void)signal;
 	write(STDOUT_FILENO, "\n", 1);
 	rl_on_new_line();
-	rl_replace_line("\n", 0);
+	rl_replace_line("", 0);
 	rl_redisplay();
 }
 
